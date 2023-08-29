ab734f875ac849b3d9ff9ca545b685c71510d407
Julian Ofori <jnbofori@gmail.com>
2023-08-29 14:00:40 +0000

    end of section 7 - mobx

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { StoreContext, store } from './app/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   // <React.StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   // </React.StrictMode>
 );
 // note: Strict mode lets components render twice (in dev) in order to detect problems and warn you
