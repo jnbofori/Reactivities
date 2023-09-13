1eb2d2ad6217e3fe789daed06e9b7fde0ddbc01d
Julian Ofori <jnbofori@gmail.com>
2023-09-13 16:23:25 +0000

    migration to vite

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -7,21 +7,22 @@ import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom";
-import LoginForm from "../../features/users/LoginForm";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <App />,
     children: [
-      { path: 'activities', element: <ActivityDashboard /> },
-      { path: 'activities/:id', element: <ActivityDetails /> },
-      { path: 'createActivity', element: <ActivityForm key='create' /> },
-      { path: 'manage/:id', element: <ActivityForm key='manage' /> },
-      { path: 'profiles/:username', element: <ProfilePage /> },
-      { path: 'login', element: <LoginForm /> },
-      { path: 'errors', element: <TestErrors /> },
+      { element: <RequireAuth />, children: [
+        { path: 'activities', element: <ActivityDashboard /> },
+        { path: 'activities/:id', element: <ActivityDetails /> },
+        { path: 'createActivity', element: <ActivityForm key='create' /> },
+        { path: 'manage/:id', element: <ActivityForm key='manage' /> },
+        { path: 'profiles/:username', element: <ProfilePage /> },
+        { path: 'errors', element: <TestErrors /> },
+      ]},
       { path: 'not-found', element: <NotFound /> },
       { path: 'server-error', element: <ServerError /> },
       { path: '*', element: <Navigate replace to='/not-found' /> },
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -6,7 +6,6 @@ import 'react-toastify/dist/ReactToastify.min.css'
 import 'react-datepicker/dist/react-datepicker.css'
 import "cropperjs/dist/cropper.css";
 import './app/layout/styles.css';
-import reportWebVitals from './reportWebVitals';
 import { StoreContext, store } from './app/stores/store';
 import { RouterProvider } from "react-router-dom";
 import { router } from './app/router/Routes';
@@ -22,8 +21,3 @@ root.render(
   // </React.StrictMode>
 );
 // note: Strict mode lets components render twice (in dev) in order to detect problems and warn you
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
